4e861c038b91b83ad4772d915fdc5cfa9c3dd66d
SM ALAMIN HOSSAIN <engrsmalaminhossain@gmail.com>
2025-11-17 18:51:24 +0800

    change habit post api

--- index.js
+++ index.js
@@ -1,51 +1,132 @@
-const { MongoClient, ServerApiVersion } = require('mongodb');
-const express = require('express');
-const cors = require('cors');
-require('dotenv').config()
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 5000;
 
 // middle ware
 
-app.use(cors())
-app.use(express.json())
-
-
-
-// const uri = "mongodb+srv://HabbitsTrackerUser:80Gk4MofiCrOyY7q@simple-crud-db.s7vfjgz.mongodb.net/?appName=simple-crud-db";
+app.use(cors());
+app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DD_USER}:${process.env.DB_PASS}@simple-crud-db.s7vfjgz.mongodb.net/?appName=simple-crud-db`;
+
+// console.log(uri);
 
 const client = new MongoClient(uri, {
   serverApi: {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+  },
 });
 
-
-
-
-
-app.get('/',(req,res)=>{
-    res.send("habbits-tracker-server is running")
-})
+app.get("/", (req, res) => {
+  res.send("habbits-tracker-server is running");
+});
 
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+    const db = client.db("habbits");
+    const habbitCollection = db.collection("habbits");
+    const usersCollection = db.collection("users");
+
+    app.post("/users", async (req, res) => {
+      const newUser = req.body;
+
+      const email = req.body.email;
+      const query = { email: email };
+      const existingUser = await usersCollection.findOne(query);
+      if (existingUser) {
+        res.send({message:"user already exists.Do not need to insert again"});
+      } else {
+        const result = await usersCollection.insertOne(newUser);
+        res.send(result);
+      }
+    });
+
+// api for public habbits
+    app.get("/habbits", async (req, res) => {
+      const cursor = habbitCollection.find();
+      const result = await cursor.toArray();
+      res.send(result);
+    });
+
+    // latest or recent habbit 
+
+    app.get("/latest-habbits",async(req,res)=>{
+      const cursor = habbitCollection.find().sort({created_At:-1}).limit(6);
+      const result = await cursor.toArray();
+      res.send(result)
+    });
+
+    // habbit details api
+
+    app.get("/habbits/:id",async(req,res)=>{
+      const id = req.params.id;
+      const query = {_id: new ObjectId(id)}
+      const result = await habbitCollection.findOne(query)
+      res.send(result)
+    });
+
+    // api for my habbits
+    app.get("/my-habbits",async(req,res)=>{
+      const email = req.query.email;
+      const query = {};
+      if(email){
+        query.email = email;
+      }
+      
+      const cursor = habbitCollection.find(query)
+      const result = await cursor.toArray();
+      res.send(result)
+
+    });
+
+    app.post("/habbits", async (req, res) => {
+      const newHabbit = req.body;
+      const habbitWithDate ={
+        ...newHabbit,created_At: new Date()
+      }
+      const result = await habbitCollection.insertOne(habbitWithDate);
+      res.send(result);
+    });
+    // update my habbit api
+
+    app.patch(`/habbits/:id`,async(req,res)=>{
+      const id = req.params.id;
+      const updateHabit = req.body;
+      const query = {_id: new ObjectId(id)}
+      const update = {
+        $set:updateHabit
+      }
+      const result = await habbitCollection.updateOne(query,update)
+      res.send(result)
+    })
+// delete api 
+    app.delete("/habbits/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await habbitCollection.deleteOne(query);
+      res.send(result);
+    });
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    // await client.close();
   }
 }
 run().catch(console.dir);
 
-app.listen(port,()=>{
-    console.log(`habbits tracker server is running on port ${port}`)
-})
+app.listen(port, () => {
+  console.log(`habbits tracker server is running on port ${port}`);
+});
